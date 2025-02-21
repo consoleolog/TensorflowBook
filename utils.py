9d64a132dc116bdd767292cfbc8a2a97ccfc3629
consolelog <leopportunity5@gmail.com>
2025-02-21 15:46:14 +0900

    fix: small

--- utils.py
+++ utils.py
@@ -9,6 +9,16 @@ def earlystopping():
         mode="min"
     )
 
+def checkpoint(chkpoint_path=f"{os.getcwd()}/checkpoint'"):
+    save_path = f"{chkpoint_path}/" + "{epoch}"
+    return tf.keras.callbacks.ModelCheckpoint(
+        filepath=save_path,
+        monitor="val_acc",
+        mode="max",
+        save_weights_only=True,
+        save_freq="epoch"
+    )
+
 def tensorboard(log_path=f"{os.getcwd()}/logs"):
     curr_time = time.strftime("%Yy%mm%dd%Hh%Mm%Ss")
     save_path = f"{log_path}/{curr_time}"
@@ -19,5 +29,9 @@ def tensorboard(log_path=f"{os.getcwd()}/logs"):
 def save_model_as_png(model, save_path=f"{os.getcwd()}/models/images"):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    tf.keras.utils.plot_model(model, f"{save_path}/{model.name}.png")
+    tf.keras.utils.plot_model(model, 
+                              to_file=f"{save_path}/{model.name}.png",
+                              show_shapes=True,
+                              show_layer_names=True)
+
     
